a2b6213b56138d09438fa7cfb2a10d408e3d65b6
jmacey <jmacey@bournemouth.ac.uk>
2016-07-28 10:52:24 +0100

    updated master

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # mayaport README
 
-MayaPort is a simple extension to allow sending the text document or selected text to the maya, you must first enable the command I've used the example from [here](https://fredrikaverpil.github.io/2013/07/15/send-mel-python-code-from-sublime-text-to-maya/) port as follows
+MayaPort is a simple extension to allow sending the text document or selected text to the maya, you must first enable the commandPort in maya. I've used the example from [here](https://fredrikaverpil.github.io/2013/07/15/send-mel-python-code-from-sublime-text-to-maya/) port as follows
 
 ```
 import maya.cmds as cmds
@@ -22,13 +22,36 @@ cmds.commandPort(name=":7002", sourceType="python")
 ## Features
 
 You can send both mel and python code via the different commands sendPythonToMaya or sendMelToMaya
+
+To get started use CMD + Shift P and rund mayaPort, this will attempt to open two sockets on the localhost, using ports 7001 and 7002 one for Mel code and one for python code.
+
+To send all the text so maya use CMD + Shift P and run sendMelToMaya or sendPythonToMaya depending upon the code you are writing. If you only wish to send a small segement of code just select the code required and use the same commands.
+
+###Keyboard Shortcuts 
+On Mac 
+Use CMD+SHIFT+P to send python code to Maya.
+Use CMD+SHIFT+M to send mel code to Maya.
+
+Windows + Linux
+
+Use CTRL+SHIFT+P to send python code to Maya.
+Use CTRL+SHIFT+M to send mel code to Maya.
+
+
 ## Requirements
 
-None 
+Autodesk Maya (Should work with all version) tested on Maya 2016 Mac and Linux.  
 
 ## Extension Settings
 
+At present the ports are coded in the file ~/.vscode/extensions/JonMacey.mayaport-0.0.1/extension.js to change the values of the ports modify the lines
 
+```
+const melPort=7001;
+const pythonPort=7002; 
+```
+
+To new values and restart the extension. 
 ## Known Issues
 
 Still work in progress
@@ -36,3 +59,8 @@ Still work in progress
 ## Release Notes
 
 Initial Alpha release 0.1
+
+## ToDo
+* See if output from maya can be caputered into the debug console.
+* Add the port values to a config file (perhaps in the package.json).
+* get user feedback and see what else can be added.
--- extension.js
+++ extension.js
@@ -2,66 +2,96 @@
 // Import the module and reference it with the alias vscode in your code below
 var vscode = require('vscode');
 var net = require('net');
+// the socket for the python ports created at startup of script
 var socketPython;
+// the socket for the mel port create at startup of script
 var socketMel;
-
+// variables for ports, can be over ridden in config if you wish to use another port, going to default to 7001 for mel and 7002 for python
+var melPort;
+var pythonPort; 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 function activate(context) {
 
+    // Use the console to output diagnostic information (console.log) and errors (console.error)
+    // This line of code will only be executed once when your extension is activated
+    console.log('"mayaport" is now active!');
+    console.log('To use make sure the following code is run in Maya python ');
+    var msg=`
+import maya.cmds as cmds
+try:
+    cmds.commandPort(name=":7001", close=True)
+except:
+    cmds.warning('Could not close port 7001 (maybe it is not opened yet...)')
+try:
+    cmds.commandPort(name=":7002", close=True)
+except:
+    cmds.warning('Could not close port 7002 (maybe it is not opened yet...)')
 
-    var disposable = vscode.commands.registerCommand('extension.openMayaPort', () => {
-        // The code you place here will be executed every time your command is executed
-        socketPython = net.createConnection({port: 7002});
-        socketMel = net.createConnection({port: 7003});
+cmds.commandPort(name=":7001", sourceType="mel")
+cmds.commandPort(name=":7002", sourceType="python")
+`;
+    console.log(msg);
 
-        vscode.window.showInformationMessage('ports opened');
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with  registerCommand
+    // The commandId parameter must match the command field in package.json
+    var disposable = vscode.commands.registerCommand('extension.openMayaPort', function () 
+    {
+        var config=vscode.workspace.getConfiguration('mayaport');
+        if(config.has("melPortID")){
+            melPort=config.get("melPortID");
+        }
+        if(config.has("pythonPortID")){
+            pythonPort=config.get("pythonPortID");
+        }
+        // create a connection to maya on port 7001 for Mel commands
+        socketMel = net.createConnection({port: melPort});
+        socketMel.on('error', function(error) { 
+            vscode.window.showErrorMessage("Unable to connect to port " + melPort + " in maya for Mel");
+         });
+        // create a connection to maya on port 7002 for python commands
+
+        socketPython = net.createConnection({port: pythonPort});
+        socketPython.on('error', function(error) { 
+            vscode.window.showErrorMessage("Unable to connect to port "+ pythonPort +" in maya for Python");
+         });
+
     });
-    
-    context.subscriptions.push(disposable);
-
-
-
-var disposable = vscode.commands.registerCommand('extension.showMayaConfig', function () {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('In Maya Do the Following' +'For Mel use  commandPort -n ":54321";'+'For Python use import maya.cmds as cmds cmds.commandPort(\'localhost:54321\')');
-});
 
     context.subscriptions.push(disposable);
+
 
 var disposable = vscode.commands.registerCommand('extension.sendPythonToMaya', function () {
 
     var editor = vscode.window.activeTextEditor;
     var selection = editor.selection;
     var text;
+    // if we have selected text only send this
     if (selection.isEmpty != true )
     {
-        text = editor.document.getText(selection);
-        
+        text = editor.document.getText(selection);     
     }
+    // otherwise send the whole document
     else
     {
         text=editor.document.getText();
     }
     socketPython.write(text);
     socketPython.write('\n');
-     //socketPython.write('print "hello maaya"');
-     console.log('attempt to write');
     });
 
     context.subscriptions.push(disposable);
 
-    var disposable = vscode.commands.registerCommand('extension.sendMelToMaya', function () {
+var disposable = vscode.commands.registerCommand('extension.sendMelToMaya', function () {
 
+    //var socket = net.createConnection({port: 7002});
     var editor = vscode.window.activeTextEditor;
     var selection = editor.selection;
     var text;
     if (selection.isEmpty != true )
     {
-        text = editor.document.getText(selection);
-        
+        text = editor.document.getText(selection);    
     }
     else
     {
@@ -69,10 +99,10 @@ var disposable = vscode.commands.registerCommand('extension.sendPythonToMaya', f
     }
     socketMel.write(text);
     socketMel.write('\n');
+     console.log('attempt to write');
     });
 
     context.subscriptions.push(disposable);
-
 
 }
 
@@ -80,7 +110,13 @@ var disposable = vscode.commands.registerCommand('extension.sendPythonToMaya', f
 
 exports.activate = activate;
 
+
+
+
+
 // this method is called when your extension is deactivated
 function deactivate() {
+    socketMel.close();
+    socketPython.close();
 }
 exports.deactivate = deactivate;
